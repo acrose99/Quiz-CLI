5c65bea52640d21b3b77060b55969c2601e0034d
jbrooks11 <49217938+jbrooks11@users.noreply.github.com>
2020-04-14 14:27:17 -0500

    Changed to work with new file format

--- answer.py
+++ answer.py
@@ -1,49 +1,81 @@
+import unittest
 import json
 
-
 def main(filename):
+    question_dict = {}
+    question_dict_wrapper = {
+        'filled': [
+
+        ],
+        'multChoice': [
+
+        ],
+        'T/F': [
+
+        ]
+    }
     with open(filename, 'r') as file_in:
-        questions = []
-        correct_or_incorrect = []
-        questioncount = 0
+        question_count = 0
         data = json.load(file_in)
-        questions_json = data['questions']
-        for question in questions_json:
-            questions.append(question['info'])
-            questioncount = questioncount + 1
-        jsonanswers = []
-        useranswers = []
-        for question in questions_json:
-            jsonanswers.append(question['Answer'])
-        upperanswers = []
-        for answer in jsonanswers:
-            upperanswers.append(answer.upper())
-        for i in range(len(questions)):
-            print(questions[i])
-            if 'options' in questions_json[i]:
-                print('Options: ' + str(questions_json[i]['options']))
-            elif questions_json[i]['Answer'] == "True" or questions_json[i]['Answer'] == "False":
-                print("True or False?")
-            answer = input("Answer: ")
-            useranswers.append(answer.upper())
-        correct_count = 0
-        tot_answer_count = 0
-        answer_output = []
-        while tot_answer_count < questioncount:
-            if useranswers[tot_answer_count] == upperanswers[tot_answer_count]:
-                correct_count = correct_count + 1
-                correct_or_incorrect.append("Correct")
-            else:
-                correct_or_incorrect.append("Incorrect")
-            user_answer_dict = {
-                "User Answer: " + str(tot_answer_count + 1): {
-                    "Question": questions[tot_answer_count],
-                    "Answer": useranswers[tot_answer_count],
-                    "Correct_or_not:": correct_or_incorrect[tot_answer_count]
-                }
+        filled_questions_json = data['filled']
+        filled_questions = []
+        filled_answers = []
+        for question in filled_questions_json:
+            filled_questions.append(question['question'])
+            filled_answers.append(question['answer'])
+            question_count = question_count + 1
+        mult_questions_json = data['multChoice']
+        mult_questions = []
+        mult_answers = []
+        for question in mult_questions_json:
+            mult_questions.append(question['question'])
+            mult_answers.append(question['answer'])
+            question_count = question_count + 1
+        tf_questions_json = data['TF']
+        tf_questions = []
+        tf_answers = []
+        for question in tf_questions_json:
+            tf_questions.append(question['question'])
+            tf_answers.append(question['answer'])
+            question_count = question_count + 1
+        for i in range(len(filled_questions)):
+            print(filled_questions[i])
+            user_answer = input("Answer: ")
+            question_dict = {
+                'question': filled_questions[i],
+                'input': user_answer,
+                'answer': filled_answers[i],
+                "correct": None
             }
-            answer_output.append(user_answer_dict)
-            tot_answer_count = tot_answer_count + 1
-        print("Your score : {}/{}".format(correct_count, questioncount))
-        with open('answers_' + filename, 'w') as file_out:
-            json.dump(answer_output, file_out, indent=4, sort_keys=True)
+            question_dict_wrapper['filled'].append(question_dict)
+        for i in range(len(mult_questions)):
+            if 'button' in mult_questions_json[i]:
+                print(mult_questions[i])
+                print('Options: ' + str(mult_questions_json[i]['button']))
+                user_answer = input("Answer: ")
+                question_dict = {
+                    'question': mult_questions[i],
+                    'button': mult_questions_json[i]['button'],
+                    'input': user_answer,
+                    'answer': mult_answers[i],
+                    'correct': None
+                }
+                question_dict_wrapper['multChoice'].append(question_dict)
+        for i in range(len(tf_questions)):
+            if 'options' in tf_questions_json[i]:
+                print(tf_questions[i])
+                print("True or False?")
+                user_answer = input("Answer: ")
+                question_dict = {
+                                    'question': tf_questions[i],
+                                    'options': {
+                                        '0': True,
+                                        '1': False,
+                                    },
+                                    'input': user_answer,
+                                    'answer': tf_answers[i],
+                                    'correct': None
+                                },
+                question_dict_wrapper['T/F'].append(question_dict)
+        with open('test.json', 'w') as file_out:
+            json.dump(question_dict_wrapper, file_out, indent=4)
